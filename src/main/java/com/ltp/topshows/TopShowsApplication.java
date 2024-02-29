6cf3fd583132ad69ecd7e0f1d36c37baae90bf8d
[Kenney Kong] <kenneykong@gmail.com>
2024-02-29 13:59:47 -0500

    first commit

---
+++ src/main/java/com/ltp/topshows/TopShowsApplication.java
@@ -0,0 +1,13 @@
+package com.ltp.topshows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopShowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopShowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ltp/topshows/TopShowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltp.topshows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopShowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
